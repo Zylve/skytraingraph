4530e67c05af406debcc044769cdcd0b916351ee
Vuk Tacic <70604613+Zylve@users.noreply.github.com>
2024-04-24 22:45:34 -0700

    Graph base

---
+++ graph.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+class Graph:
+    graph = defaultdict(dict)
+
+    def __init__(self) -> None:
+        pass
+
+    def add_connection(self, start_station, end_station, cost) -> None:
+        pass
+
+    def get_connections(self, station):
+        pass
